22f64b8426ffe449bc9ee4b2331866640fcabe60
MlrDevs97 <mlr.devs.97@gmail.com>
2024-05-14 09:55:57 +0200

    creating header

---
+++ src/app/components/shared/header/contact-social/contact-social.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-social',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-social.component.html',
+  styleUrl: './contact-social.component.css'
+})
+export class ContactSocialComponent {
+
+}
--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ContactSocialComponent } from './contact-social/contact-social.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavMenuComponent],
+  imports: [NavMenuComponent, ContactSocialComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
